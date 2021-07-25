f91d99d8b9a7d7ba03b8c81b17826b18b4d044c9
Abdallah Abu Khurma <abdallahabukhurma@gmail.com>
2021-07-25 18:02:27 +0300

    try and error

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.get('/info', (req, res) => {
   res.json({name: 'Abdallah', age: 24});
 });
 
-app.get('/data', (req, res) => {
-  res.json(Data);
-});
+// app.get('/data', (req, res) => {
+//   res.json(Data);
+// });
 
 app.listen(PORT, () => {
   console.log(`server is listening to ${PORT}`);
